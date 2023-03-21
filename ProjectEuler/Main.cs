46e85b6461def1c88f5fc6dd7e635bffb4b0b8f6
Michel Portzert <>
2023-03-21 20:01:45 +0100

    Problem 22

--- ProjectEuler/Main.cs
+++ ProjectEuler/Main.cs
@@ -8,17 +8,30 @@ namespace ProjectEuler
 {
     internal class Program
     {
+        public static string s_AssetsPath = "ProjectEuler.Assets.";
+
         static void Main(string[] args)
         {
-            var sum = 0;
-            var limit = 10000;
-            var amicables = MathUtils.FindAmicableNumbers(limit);
-            foreach (var amicable in amicables)
+            var resourceName = s_AssetsPath + "p022_names.txt";
+            var namesStr = IOUtils.LoadFileContent(resourceName);
+
+            namesStr = namesStr.Replace("\"", "");
+            var names = namesStr.Split(',');
+            var sortedNames = names.OrderBy(x => x).ToArray();
+
+            double total = 0;
+            var count = sortedNames.Length;
+            for (int i = 0; i < count; i++)
             {
-                sum += amicable;
+                var nameSum = 0;
+                var name = sortedNames[i];
+                foreach (char c in name)
+                    nameSum += (c - 'A') + 1;
+
+                total += nameSum * (i + 1);
             }
 
-            Console.WriteLine(sum);
+            Console.WriteLine(total);
         }
     }
 }
---
+++ ProjectEuler/Utils/IOUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace ProjectEuler
+{
+    public static class IOUtils
+    {
+        public static string LoadFileContent(string resourceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            using (Stream? stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream!))
+            {
+                string result = reader.ReadToEnd();
+                return result;
+            }
+        }
+    }
+}
+
